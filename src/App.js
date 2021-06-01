71e41fa59d1c8de25e20cb08a977597d718052d7
alekry78 <krysaleksander1@gmail.com>
2021-06-02 00:00:04 +0200

    handling checkboxes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React, {useEffect, useState} from 'react';
-import {AppContainer, Avatar, Details, Mail, Name, UserContainer} from "./App.styles";
+import {AppContainer, Avatar, Details, Image, List, Mail, Name, Search, UserContainer} from "./App.styles";
 
 const App = () => {
-    const[data,setData]=useState(false);
-    const[users,setUsers]=useState([]);
-    useEffect(() =>{
+    const [data, setData] = useState(false);
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
         fetch("https://teacode-recruitment-challenge.s3.eu-central-1.amazonaws.com/users.json")
             .then(response => response.json())
             .then(user => {
-                setUsers(user);
+                const User = [];
+                for (let i = 0; i < Array.from(user).length; i++) {
+                    User.push({
+                        id: Array.from(user)[i].id,
+                        first_name: Array.from(user)[i].first_name,
+                        last_name: Array.from(user)[i].last_name,
+                        email: Array.from(user)[i].email,
+                        gender: Array.from(user)[i].gender,
+                        avatar: Array.from(user)[i].avatar,
+                        checked: false
+                    })
+                }
+                setUsers(User)
                 setData(true);
             });
-    },[])
-    if(data===false){
+    }, [])
+    if (data === false) {
         console.log("Jeszcze nie mam danych");
-    }else{
-        users.sort((a,b)=>{
+    } else {
+        users.sort((a, b) => {
             const nameA = a.last_name.toUpperCase();
             const nameB = b.last_name.toUpperCase();
             let comparison = 0;
@@ -27,33 +39,54 @@ const App = () => {
             return comparison;
         })
     }
-    return(
+    const handleCheck = (ID) => {
+        const Users = [...users];
+        Users[Users.findIndex(i => i.id === ID)].checked = !Users[Users.findIndex(i => i.id === ID)].checked;
+        setUsers(Users);
+        console.log(users.filter(el => el.checked === true))
+    }
+    const [name,setName] =useState("");
+    const handleSearch = (e)=>{
+        setName(e.target.value)
+        const Users = [...users];
+
+    }
+    return (
         <AppContainer>
             <h1>React Challenge</h1>
-            {data ? users.map(el=>{
-                    return(
-                        <UserContainer>
-                            <Avatar>
-                                {el.first_name.charAt(0)}
-                                {el.last_name.charAt(0)}
-                            </Avatar>
-                            <Details>
-                                <Name>
-                                    {el.first_name}
-                                    {el.last_name}
-                                </Name>
-                                <Mail>
-                                    {el.email}
-                                </Mail>
-                            </Details>
-                        </UserContainer>
-                    )
-                })
+            <Search type="text" value={name} onChange={(e)=>handleSearch(e)}/>
+            {data ?
+                <List>
+                    {users.map(el => {
+                        return (
+                            <li key={el.id}>
+                                <UserContainer onClick={() => handleCheck(el.id)}>
+                                    {el.avatar ? <Image src={el.avatar} alt="Logo"/> :
+                                        <Avatar>
+                                            {el.first_name.charAt(0)} {el.last_name.charAt(0)}
+                                        </Avatar>
+                                    }
+                                    <Details>
+                                        <Name>
+                                            {el.first_name}
+                                            {el.last_name}
+                                        </Name>
+                                        <Mail>
+                                            {el.email}
+                                        </Mail>
+                                    </Details>
+                                    <input type="checkbox" name="checkbox" checked={el.checked}
+                                           onChange={() => handleCheck(el.id)}/>
+                                </UserContainer>
+                            </li>
+                        )
+                    })}
+                </List>
                 :
                 <h1>Laduje dane</h1>
             }
         </AppContainer>
     )
-};
+}
 
 export default App
--- src/App.styles.js
+++ src/App.styles.js
@@ -1,50 +1,72 @@
 import styled from "styled-components";
 
 export const AppContainer = styled.main`
-  html{
-    padding:0;
-    margin:0;
+  html {
+    padding: 0;
+    margin: 0;
   }
-  width:100vw;
-  height:100vh;
-  padding:0;
-  margin:0;
+
+  width: 100vw;
+  height: 100vh;
+  padding: 0;
+  margin: 0;
 `
 export const UserContainer = styled.div`
-  width:100vw;
-  height:45px;
-  border-bottom:1px solid dimgrey;
-  background-color:lightgray;
-  display:flex;
-  justify-content:flex-start;
-  align-items:center;
-  padding:5px;
+  width: 100vw;
+  height: 45px;
+  border-bottom: 1px solid dimgrey;
+  background-color: lightgray;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 5px;
 `
 export const Avatar = styled.div`
-  display:flex;
-  justify-content:center;
-  align-items:center;
-  font-size:20px;
-  color:grey;
-  background-color:white;
-  border-radius:50%;
-  width:40px;
-  height:40px;
-  border:1px solid grey;
-  margin-right:5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+  color: grey;
+  background-color: white;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  border: 1px solid grey;
+  margin-right: 5px;
+`
+export const Image = styled.img`
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  border: 1px solid grey;
+  margin-right: 5px;
 `
 export const Details = styled.div`
-  display:flex;
-  flex-direction:column;
-  justify-content:center;
-  align-items:flex-start;
-  margin:0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  margin: 0;
 `
 export const Name = styled.span`
-  font-size:20px;
-  color:black;
+  font-size: 20px;
+  color: black;
+  font-weight: bold;
 `
 export const Mail = styled.span`
-  font-size:15px;
-  color:grey;
+  font-size: 15px;
+  color: dimgray;
+`
+export const List = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+`
+export const Search = styled.input`
+  width: 100vw;
+  height: 50px;
+  font-size: 40px;
+  color: black;
+  outline:none;
+  border-top:1px solid black;
 `
